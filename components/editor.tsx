5f686cf634cd73c3ac28f9cd232dbfa42b637413
Anjal Patel <patel.anjal567@gmail.com>
2024-04-25 15:00:33 +0530

    fix13

--- components/editor.tsx
+++ components/editor.tsx
@@ -27,7 +27,7 @@ interface EditorProps{
     const editor: BlockNoteEditor = useBlockNote ({
         initialContent:initialContent ? 
         JSON.parse(initialContent) as PartialBlock[] : undefined,
-        onChange: (editor) => {
+        onEditorContentChange: (editor) => {
           onChange(JSON.stringify(editor.topLevelBlocks,null,2));
         },
         uploadFile: handleUpload
